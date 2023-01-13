1fe932cd0539b6cf783ca5ad9ac03ad0a5171f46
fedorbor <112242236+fedorbor@users.noreply.github.com>
2023-01-13 18:10:28 +0200

    sd

---
+++ js/dz.js
@@ -0,0 +1,69 @@
+// 1
+console.log("1");
+let str = "Привіт";
+let num = 123;
+let flag = true;
+let txt = "true";
+console.log(typeof(str) );
+console.log(typeof(num) );
+console.log(typeof(flag) );
+console.log(typeof(txt) );
+// 2
+console.log("2");
+let x1= true;
+let x2="true";
+let x3=12;
+let x4="hi";
+let x5="";
+const y1= false;
+const y2="false";
+const y3=21;
+const y4="hello";
+const y5=null;
+// 3
+console.log("3");
+const userName = prompt("введіть імя користувача");
+console.log(userName);
+// 4
+console.log("4");
+const myAge=14;
+const myFriendAge=15;
+alert(myAge+myFriendAge)
+// 5
+console.log("5");
+let a=3;
+let b=9;
+console.log(a+b)
+// 6
+console.log("6");
+const a1 = 5+3;
+const a2 = 5-3;
+const a3 = 3-5;
+const a4 = 5*3;
+const a5 = 5/3;
+console.log(a1 ,a2 ,a3 ,a4 ,a5);
+// 7    
+console.log("7");
+const a6 = 5 % 3;
+const a7 = 3 % 5; 
+const a8 = 5 + '3';
+const a9 =  '5' - 3;
+const a10 = 75 + 'кг';
+alert(a6);
+alert(a7);
+alert(a8);
+alert(a9);
+alert(a10);
+// 8    
+console.log("8");
+const height = 23;
+const width = 10;
+let s =height*width;
+console.log(s)
+// 9    
+console.log("9");
+const heightC = 10;
+const dC=4;
+const rC=dC/2;
+const v = rC*rC*3.14*heightC;
+console.log(v);
